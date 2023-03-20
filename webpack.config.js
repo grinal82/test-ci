5fd93342e424d8d24cef0e549aec80902ce4001c
girnal82 <grinal82@gmail.com>
2023-03-20 22:02:30 +0300

    added webpack and its config and script

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,34 @@
-const path = require("path");
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable linebreak-style */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"), // каталог для результатов сборки
-        filename: "index.js", // имя файла с результатами сборки (должно совпадать с entry point в шаге 1)
-        libraryTarget: "umd", // UMD (Universal Module Definition https://github.com/umdjs/umd) - шаблон, который позволяет использовать RequireJS и браузер
-        libraryExport: "default", // экспортируется имя default
-        globalObject: "this", // что принимать за глобальный объект, иначе сгенерируется window, а его, как вы знаете, на платформе Node.js нет
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-        ],
-    },
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
